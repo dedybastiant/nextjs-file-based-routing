157612f59df8c183b62a6cd5fe1b9529f4f1f402
dedybastiant <dedybastian.t@gmail.com>
2023-07-11 14:08:53 +0700

    learn about nested dynamic path and route

---
+++ pages/clients/[id]/[clientProjectid].js
@@ -0,0 +1,9 @@
+function SelectedClientProjectPage() {
+  return (
+    <div>
+      <h1>The Project Page for a Specific project for a Selected Client</h1>
+    </div>
+  );
+}
+
+export default SelectedClientProjectPage;
